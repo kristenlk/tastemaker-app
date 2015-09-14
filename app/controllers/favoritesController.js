eb978edff4ac56525834f42232111eaffd9726a8
kristenlk <klkehlenbeck@gmail.com>
2015-09-14 01:43:00 -0400

    Adds a previous restaurant button and adds error handling for favorites.

--- app/controllers/favoritesController.js
+++ app/controllers/favoritesController.js
@@ -9,6 +9,7 @@
 
     function init(){
       favoritesFactory.getFavorites().then(function(favorites){
+        console.log(vm.favorites);
         for (var i = 0; i < vm.favorites.length; i++) {
           vm.favorites[i].display_address = JSON.parse(vm.favorites[i].display_address);
         }
